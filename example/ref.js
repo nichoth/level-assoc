05de44b7e645dfc9f012641d619a5087bb3b88d6
James Halliday <mail@substack.net>
2013-08-18 22:37:09 -0700

    hacky pairings based on the type key

--- example/ref.js
+++ example/ref.js
@@ -11,11 +11,12 @@ assoc.add('hacker').belongsTo('hackerspace');
 assoc.add('tool').belongsTo('hackerspace');
 
 db.batch(require('./data.json').map(function (row) {
+assoc._PUT(row.key, row.value);
     return { type: 'put', key: row.key, value: row.value };
 }), ready);
 
 function ready () {
     assoc.get('sudoroom', function (err, room) {
-        console.log(room);
+        room.hackers().on('data', console.log);
     });
 }
--- index.js
+++ index.js
@@ -1,41 +1,94 @@
 var pathway = require('pathway');
+var bytewise = require('bytewise');
+var Transform = require('readable-stream/transform');
+var foreignKey = require('foreign-key');
 
 module.exports = Assoc;
 function Assoc (db) {
     if (!(this instanceof Assoc)) return new Assoc(db);
     this.db = db;
-    this._types = {};
+    this._sublevels = {};
+    this._foreign = {};
+    this._has = [];
+    this._hasKeys = {};
 }
 
-Assoc.prototype.add = function (name) {
-    var t = new Type(name);
-    this._types[name] = t;
-    return t;
+Assoc.prototype.add = function (topType) {
+    if (typeof topType === 'string') topType = [ 'type', topType ];
+    var key = topType.join('\0');
+    
+    this._foreign[key] = foreignKey(topType);
+    this._sublevels[key] = this.db.sublevel('assoc-' + key);
+    this._hasKeys[key] = {};
+    
+    var self = this;
+    return new Type(function (k, type) {
+        self._has.push([ type, k, topType ]);
+        self._hasKeys[key][k] = type;
+    });
 };
 
-Assoc.prototype.get = function (name, cb) {
+Assoc.prototype._PUT = function (key, value) {
+    if (!value) return;
+    if (this._foreign[value.type]) {
+        // ...
+    }
+    
+    for (var i = 0, li = this._has.length; i < li; i++) {
+        var ts = this._has[i][0];
+        var cur = value;
+        for (var cur, j = 0, lj = ts.length - 1; j < lj; j++) {
+            cur = cur[ts[j]];
+            if (cur === undefined) break;
+        }
+        if (j !== lj || cur !== ts[j]) continue;
+        console.log(ts, value);
+    }
+    
+    //this._sublevels[value.type].put(key, value);
+    //console.log('rkey=', [ key, value ]);
+    //this.db.put(key, value);
+};
+
+Assoc.prototype.get = function (topKey, cb) {
     var self = this;
-    this.db.get(name, function (err, row) {
+    this.db.get(topKey, function (err, row) {
         if (err) return cb(err);
-        var t = self._types[row.type];
         
-        Object.keys(t._has).forEach(function (key) {
-            var type = t._has[key];
-            row[key] = new Collection(key, type);
+        return; // whatever
+        var t = self._hasKeys[row.type];
+        Object.keys(t._hasKeys).forEach(function (key) {
+            var type = t._hasKeys[key];
+            
+            row[key] = function () {
+                var opts = {
+                    /*
+                    start: bytewise.encode(),
+                    end: ''
+                    */
+                };
+                var tr = new Transform({ objectMode: true });
+                tr._transform = function (row, enc, next) {
+                    // ...
+                };
+                tr._flush = function (next) {
+                    next();
+                };
+                return self.db.createReadStream(opts).pipe(tr);
+            };
         });
         
         cb(null, row);
     });
 };
 
-function Type (type) {
-    this.type = type;
-    this._has = {};
+function Type (cb) {
+    this._cb = cb;
 }
 
 Type.prototype.hasMany = function (key, type) {
     if (typeof type === 'string') type = [ 'type', type ];
-    this._has[key] = type;
+    this._cb(key, type);
     return this;
 };
 
@@ -47,13 +100,4 @@ Type.prototype.belongsTo = function (type, key) {
         + ' Specify a key.'
     );
     return this;
-};
-
-function Collection (key, type) {
-    this.key = key;
-    this.type = type;
-}
-
-Collection.prototype.inspect = function () {
-    return '<Collection[ ' + this.type.join(', ') + ' ]>';
 };
